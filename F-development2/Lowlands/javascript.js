3f7eb624c14ca821d8db81e6dae6885b968a59d5
Sang Dinh <55748789+dinhs-frontend@users.noreply.github.com>
2022-02-08 17:23:09 +0100

    Add files via upload

---
+++ F-development2/Lowlands/javascript.js
@@ -0,0 +1,22 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+//---------------Bron--------------//
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible
